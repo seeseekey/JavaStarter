f9db155477a20c4b0f62c2d73f1d3b6b74d5f0e6
seeseekey <kontakt@seeseekey.net>
2019-03-26 18:59:06 +0100

    Add logger to Starter class.

--- src/main/java/org/example/starter/Starter.java
+++ src/main/java/org/example/starter/Starter.java
@@ -1,9 +1,14 @@
 package org.example.starter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Starter {
+
+    private static Logger logger = LoggerFactory.getLogger(new Exception().fillInStackTrace().getStackTrace()[0].getClassName());
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
+        logger.info("Hello, World!");
     }
 }
